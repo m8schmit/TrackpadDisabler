efcdfa6412fbfcd7264959351413334181f9eed1
Sho <schmit.michel.fr@gmail.com>
2016-03-30 11:43:44 +0200

    fix

--- touchpad_toggle.sh
+++ touchpad_toggle.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-enabled=$1;
 touchpad_id=$(xinput | grep -i "touchpad" | cut -f2 | cut -d '=' -f2);
-mouse_id=$(xinput | grep -i "mouse" | grep -i 'pointer' | cut -f2 | cut -d '=' -f2);
-mouse_prop=$(xinput list-props $mouse_id | grep -i "velocity" | cut -f2 | cut -d '(' -f2 | cut -d ')' -f1 );
 mouse_tmp= "error";
 
-if $enabled
+if $1
 	then
 	xinput set-prop $touchpad_id "Device Enabled" 1 | notify-send -i /usr/local/bin/padlock_closed.png "The touchpad is now enabled." "$(cat /tmp/mouse_name) unplugged"
 	rm /tmp/mouse_name
